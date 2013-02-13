d8c7ac189a5ef5faa6568e9d358607da63921d95
Jay Marcyes <github@marcyes.com>
2013-02-12 17:35:35 -0800

    initial commit, breaking this out into a reusable module and finally making it work

---
+++ testmod1.py
@@ -0,0 +1,6 @@
+import testmod2
+
+happy = range(5)
+
+def click_whirr():
+    return testmod2.click_whirr()
---
+++ testmod2.py
@@ -0,0 +1,12 @@
+#import time
+#from os import path
+from profiler import start
+#from time import time as timestamp
+
+#pout.v(timestamp())
+
+def click_whirr():
+
+    import time
+
+    return time.time()
